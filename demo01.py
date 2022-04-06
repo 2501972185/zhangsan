d760960b1c8d659978ceff9fac4a3ef4649f4fab
lsx <2501972185@qq.com>
2022-04-06 19:27:28 +0800

    第一次向github提交代码

---
+++ demo01.py
@@ -0,0 +1,37 @@
+#1
+# print("你好")
+# print(())#元组  tuple
+# print([])#数组  list
+# print({})#字典  dict
+
+# #2.字符串
+# print("哈哈"+"喜喜")  #拼接(同类型)
+# print("哈哈"*10)     #乘
+# print(((1+2)*100-99)/2)
+# print(2>3)
+
+#3.1默认input输入的都为字符串类型
+# a=input()
+# b=input()
+# print("输入内容：",a+b)
+#3.2
+# a=float(input())
+# b=float(input())
+# print("输入内容：",a+b)
+
+#4.类型转换  type
+# #布尔值 True 、False
+# print(type(233))
+# print(type(True))
+# print(type(False))
+# print(type(()))
+# print(type([]))
+# print(type({}))
+
+#5.len()
+# a='hahhahah  asa'
+# print(len(a))
+
+a=len(input())
+b=len(input())
+print("长度为：",a+b)
